55f1d1073b8e2b751b63e49853b2fc1916131300
Yevheniia Silenko <prencezza12@gmail.com>
2024-02-28 17:46:47 +0200

    task1 added
    Signed-off-by: Yevheniia Silenko <prencezza12@gmail.com>

---
+++ HW5/task1.py
@@ -0,0 +1,54 @@
+import random
+
+numbers = []
+ARR_SIZE = 10
+for _ in range(ARR_SIZE):
+    numbers.append(random.randint(-10, 10))
+print(numbers)
+
+negative_numbers_sum = 0
+even_numbers_sum = 0
+odd_numbers_sum = 0
+result_sum = 0
+for number in numbers:
+    if number < 0:  # Суму негативних чисел;
+        negative_numbers_sum += number
+    if number % 2 == 0:  # Суму парних чисел;
+        even_numbers_sum += number
+    if number % 2 != 0:  # Суму непарних чисел;
+        odd_numbers_sum += number
+    if number % 3 == 0:  # Добуток елементів з кратними індексами 3;
+        result_sum += number
+
+print(negative_numbers_sum)
+print(even_numbers_sum)
+print(odd_numbers_sum)
+print(result_sum)
+
+min_value_index = numbers.index(min(numbers))
+max_value_index = numbers.index(max(numbers))
+
+if min_value_index > max_value_index:
+    min_value_index, max_value_index = max_value_index, min_value_index
+
+result = 0
+for i in range(min_value_index + 1, max_value_index):
+    result += numbers[i]
+
+print(result)
+
+first_positive_index = last_positive_index = 0
+for i in range(ARR_SIZE):
+    if numbers[i] > 0:
+        first_positive_index = i
+    break
+
+for i in range(ARR_SIZE - 1, -1, -1):
+    if numbers[i] > 0:
+        last_positive_index = i
+        break
+print(first_positive_index, last_positive_index)
+
+result = 0
+for i in range(first_positive_index + 1, last_positive_index):
+    result += numbers[i]
